dc5e7b2f3ecda07318f55992912b6438084ddd91
Daniele <Daniele@DESKTOP-GIBR9UI>
2023-11-19 14:09:41 +0100

    створив метод в куб в новій вітці

--- Program.cs
+++ Program.cs
@@ -30,5 +30,19 @@ namespace Git_Repositories
 
             Console.WriteLine($"Відповідь: {result}");
         }
+        static void Сube()
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(1251);
+            Console.InputEncoding = Encoding.GetEncoding(1251);
+            Console.WriteLine("Виберіть число для виведення в куба:");
+            int number = int.Parse(Console.ReadLine());
+
+            int result = 1;
+
+            for (int i = 0; i < 3; i++)
+                result *= number;
+
+            Console.WriteLine($"Відповідь: {result}");
+        }
     }
 }
